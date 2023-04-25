8e641836ba4368e2aee51cfb712e680520750f81
Sikandar Saleem <sikandarsaleem2000@gmail.com>
2023-04-25 17:58:35 +0500

    add customers

--- app/services/stripe_service.rb
+++ app/services/stripe_service.rb
@@ -2,4 +2,14 @@ class StripeService
   def initialize
     Stripe.api_key = ENV['STRIPE_SECRET_KEY']
   end
+
+  def find_or_create_customer(customer)
+    stripe_customer = if customer.stripe_customer_id.present? 
+                        Stripe::Customer.retrieve(customer.stripe_customer_id)
+                      else 
+                        Stripe::Customer.create(name: customer.name, email: customer.email, phone: customer.phone)
+                      end
+    customer.update(stripe_customer_id: stripe_customer.id) unless customer.stripe_customer_id.present?
+    stripe_customer
+  end
 end
