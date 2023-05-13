f8267553c9b96548ecaa817c186e62d44edc0cde
Sikandar Saleem <sikandarsaleem2000@gmail.com>
2023-05-14 03:37:48 +0500

    add products

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :products
   root "orders#new"
 
   resources :orders, only: [:new, :create]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_25_160820) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_13_223514) do
   create_table "customers", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -27,6 +27,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_25_160820) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["customer_id"], name: "index_orders_on_customer_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "orders", "customers"
